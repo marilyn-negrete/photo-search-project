d550008e6cd2f3a26e9e9a62ea26fe87502bc0eb
Marilyn Negrete <marilyn.negrete@globant.com>
2023-12-21 14:37:59 -0600

    redirect Link in Home page to Search page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 'use client';
 import React from "react";
-import RadioLink from "./ui/button/RadioLink";
+import RadioLink from "./ui/Links/RadioLink";
 
 export default function Home() {
   return (
     <>
      <h1>Home Page</h1>
-     <RadioLink label="Search" url="/feed"/>
+     <RadioLink label="Search" url="/local-photographers"/>
     </>
   )
 }
