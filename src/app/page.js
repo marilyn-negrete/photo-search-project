83765aaa9bade39db226d5b2ae6fd8354c16925e
Marilyn Negrete <marilyn.negrete@globant.com>
2023-12-21 13:29:54 -0600

    create RadioButton component

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,11 @@
+import React from "react";
+import RadioButton from "./ui/button/RadioButton";
+
 export default function Home() {
-  return (<h1>Home Page</h1>
+  return (
+    <>
+     <h1>Home Page</h1>
+     <RadioButton label="Search"/>
+    </>
   )
 }
---
+++ src/app/ui/button/RadioButton.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RadioButton = (props) => {
+    const { label } = props;
+
+    return(
+        <button>{label}</button>
+    )
+}
+
+export default RadioButton;
