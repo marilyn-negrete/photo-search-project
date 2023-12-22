d96d31fbec05d10b766748084f57a9685b2d1b40
Marilyn Negrete <marilyn.negrete@globant.com>
2023-12-22 14:59:33 -0600

    add Home background image

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,27 @@
 'use client';
+import Image from "next/image";
+import thumbnail from "../../public/photography-thumbnail.jpeg";
 import React from "react";
 import RadioLink from "./ui/Links/RadioLink";
+import styled from "styled-components";
+
+
+const StyledImage = styled(Image)`
+  z-index: -1;
+`;
 
 export default function Home() {
   return (
-    <>
-     <h1>Home Page</h1>
-     <RadioLink label="Search" url="/local-photographers"/>
-    </>
+    <section>
+      <StyledImage
+        src={thumbnail}
+        alt="Thumbnail"
+        layout="fill"
+        objectFit="cover"
+      />
+      <h1>Home Page</h1>
+      <RadioLink label="Search" url="/local-photographers"/>
+    </section>
+    
   )
 }
