6dac3faa35ae301fd69d33bc1c0bfce1ebedbe66
Marilyn Negrete <marilyn.negrete@globant.com>
2024-01-03 17:24:10 -0600

    add nav options and nav styles

--- src/app/components/Nav/Nav.js
+++ src/app/components/Nav/Nav.js
@@ -1,15 +1,70 @@
-const Nav = () => {
-    return(
-        <nav>
-            <ul>
-                <li>Option 1</li>
-                <li>Option 2</li>
-                <li>Option 3</li>
-                <li>Option 4</li>
-                <li>Option 5</li>
-            </ul>
-        </nav>
-    )
- }
+import { StyledNav } from "./Nav.styled";
+import Link from "next/link";
+import Image from "next/image";
+import home from "../../../../public/home.png";
+import camera from "../../../../public/camera.png";
+import location from "../../../../public/location.png";
+import bell from "../../../../public/bell.png";
+import user from "../../../../public/user.png";
 
- export default Nav;
+const Nav = () => {
+    return (
+        <StyledNav>
+            <ul>
+                <li>
+                    <Link href="/">
+                        <Image
+                            alt="home-option"
+                            src={home}
+                            width={25}
+                            height={25}
+                        />
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/local-photographers">
+                        <Image
+                            alt="location-option"
+                            src={location}
+                            width={25}
+                            height={25}
+                        />
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/camera">
+                        <Image
+                            alt="camera-option"
+                            src={camera}
+                            width={25}
+                            height={25}
+                        />
+                    </Link>
+                </li>
+                <li>
+                    <Link href="/feed">
+                        <Image
+                            alt="bell-option"
+                            src={bell}
+                            width={25}
+                            height={25}
+                        />
+                    </Link>
+
+                </li>
+                <li>
+                    <Link href="/profile/1">
+                        <Image
+                            alt="user-option"
+                            src={user}
+                            width={25}
+                            height={25}
+                        />
+                    </Link>
+                </li>
+            </ul>
+        </StyledNav>
+    )
+}
+
+export default Nav;
---
+++ src/app/components/Nav/Nav.styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StyledNav = styled.nav`
+    background-color: #F4DFCA;
+
+    ul {
+        padding: 1rem;
+        margin: 0;
+        list-style-type: none;
+        display: flex;
+        justify-content: space-between;
+
+        li {
+            text-decoration: none;
+        }
+    }
+`;
