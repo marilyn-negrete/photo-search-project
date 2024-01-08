5944a3beeeda4aea867db11d60263f4289c1b14e
Marilyn Negrete <marilyn.negrete@globant.com>
2024-01-08 14:23:03 -0600

    remove unused code

--- src/app/Global.styled.js
+++ src/app/Global.styled.js
@@ -2,4 +2,5 @@ import styled from "styled-components";
 
 export const Global = styled.body`
     margin: 0;
+    min-height: 100vh;
 `;
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,17 +2,14 @@
 import { Global } from './Global.styled';
 import Nav from './components/Nav/Nav';
 import StyledComponentsRegistry from './lib/registry'
-import { Inter } from 'next/font/google';
 import { usePathname } from 'next/navigation'
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
 
   return (
     <html lang="en">
-      <Global className={inter.className}>
+      <Global>
       {pathname !== '/' ? <Nav /> : null}
         <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
       </Global>
