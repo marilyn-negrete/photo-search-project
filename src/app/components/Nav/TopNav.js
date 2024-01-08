4ac176f013a067a7778300bffc03c184afdb23a3
Marilyn Negrete <marilyn.negrete@globant.com>
2024-01-08 16:36:39 -0600

    add link in logo to redirect to the home page

--- src/app/components/Nav/TopNav.js
+++ src/app/components/Nav/TopNav.js
@@ -2,11 +2,14 @@ import { StyledTopNav } from "./Nav.styled";
 import Image from "next/image";
 import logo from "../../../../public/photo-search-logo.png";
 import hamburguer from "../../../../public/hamburguer-menu.png";
+import Link from "next/link";
 
 const TopNav = () => {
     return(
         <StyledTopNav>
-            <Image width={30} height={30} src={logo} alt="photo-search-logo"/>
+            <Link href="/">
+                <Image width={30} height={30} src={logo} alt="photo-search-logo"/>
+            </Link>
             <h4>PhotoSearch</h4>
             <Image width={20} height={20} src={hamburguer} alt="hamburguer-menu"/>
         </StyledTopNav>
