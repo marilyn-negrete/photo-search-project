b3608596c7f425ddee5bad50ba7b17308366257e
Marilyn Negrete <marilyn.negrete@globant.com>
2024-01-16 09:50:55 -0600

    add collections dummy data

---
+++ src/app/lib/collections.js
@@ -0,0 +1,53 @@
+export const collections = [
+    {
+        id: 1,
+        title: 'Portraits',
+        thumbnail: '',
+        description: 'Lorem ipsum dolor sit amet consectetur adipiscing, elit rutrum euismod cubilia fusce, tortor facilisi aliquam proin hac.',
+        items: [
+            { id: 11, src: '', captionText: 'Lorem ipsum dolor sit amet consectetur adipiscing.' },
+            { id: 12, src: '', captionText: 'Lorem ipsum dolor sit amet consectetur adipiscing.' },
+            { id: 10, src: '', captionText: 'Lorem ipsum dolor sit amet consectetur adipiscing.' },
+        ]
+    },
+    {
+        id: 2,
+        title: 'Nature',
+        thumbnail: '',
+        description: 'Purus parturient natoque sem per conubia dictum curae ullamcorper ultricies commodo.',
+        items: [
+            { id: 13, src: '', captionText: 'Lorem ipsum dolor sit amet consectetur adipiscing.' },
+            { id: 14, src: '', captionText: 'Lorem ipsum dolor sit amet consectetur adipiscing.' },
+        ]
+    },
+    {
+        id: 3,
+        title: 'Black & white',
+        thumbnail: '',
+        description: 'Rhoncus ullamcorper risus nostra metus vivamus nascetur habitant.',
+        items: [
+            { id: 15, src: '', captionText: 'Lorem ipsum dolor sit amet consectetur adipiscing.' },
+            { id: 16, src: '', captionText: 'Lorem ipsum dolor sit amet consectetur adipiscing.' },
+        ]
+    },
+    {
+        id: 4,
+        title: 'Events',
+        thumbnail: '',
+        description: 'Parturient quis laoreet in tempus tortor imperdiet nulla vulputate duis, quisque sagittis dis nam habitant purus urna nisl libero.',
+        items: [
+            { id: 17, src: '', captionText: 'Lorem ipsum dolor sit amet consectetur adipiscing.' },
+            { id: 18, src: '', captionText: 'Lorem ipsum dolor sit amet consectetur adipiscing.' },
+        ]
+    },
+    {
+        id: 5,
+        title: 'Fine Art',
+        thumbnail: '',
+        description: 'Auctor dapibus blandit ad integer ridiculus curabitur, felis cubilia ornare aliquam platea congue pulvinar, eu pellentesque luctus convallis dignissim.',
+        items: [
+            { id: 19, src: '', captionText: 'Lorem ipsum dolor sit amet consectetur adipiscing.' },
+            { id: 20, src: '', captionText: 'Lorem ipsum dolor sit amet consectetur adipiscing.' },
+        ]
+    }
+]
