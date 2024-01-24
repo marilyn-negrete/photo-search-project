6b4470fec67f18af6b4ac6ba0508e7466e49c218
Marilyn Negrete <marilyn.negrete@globant.com>
2024-01-24 16:59:05 -0500

    place RadioButton component in profile page

--- src/app/profile/[id]/page.js
+++ src/app/profile/[id]/page.js
@@ -1,3 +1,11 @@
+'use client'
+import RadioButton from "@/app/components/Buttons/RadioButton";
+
 export default function Page({params}) {
-    return <h1>Profile Page: {params.id}</h1>
+    return (
+        <>
+            <h1>Profile Page: {params.id}</h1>
+            <RadioButton />
+        </>
+    )
 }
