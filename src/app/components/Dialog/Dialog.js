b39bced1014a81ce5a7ad8eeb50e49ce69256391
Marilyn Negrete <marilyn.negrete@globant.com>
2024-01-26 14:29:18 -0500

    add dialog backdrop element

--- src/app/components/Dialog/Dialog.js
+++ src/app/components/Dialog/Dialog.js
@@ -1,20 +1,25 @@
-import { DialogWrapper, DialogContent, DialogBody, DialogHead, DialogActions } from "./Dialog.styled";
+import { DialogWrapper, DialogContent, DialogBody, DialogHead, DialogActions, DialogBackdrop } from "./Dialog.styled";
 import Typography from "../Typography/Typography";
 
 const Dialog = (props) => {
 const { children, dialog } = props;
 
     return(
-        <DialogWrapper isOpen={dialog.isOpen}>
-            <DialogContent>
-                <DialogHead>
-                    <Typography variant="h4" label={dialog.title} />
-                </DialogHead>
-                <DialogBody>
-                    { children}
-                </DialogBody>
-            </DialogContent>
-        </DialogWrapper>
+        <>
+            <DialogWrapper isOpen={dialog.isOpen}>
+                <DialogContent>
+                    <DialogHead>
+                        <Typography variant="h4" label={dialog.title} />
+                    </DialogHead>
+                    <DialogBody>
+                        { children}
+                    </DialogBody>
+                </DialogContent>
+            </DialogWrapper>
+            {dialog.isOpen ? <DialogBackdrop></DialogBackdrop> : null}
+            
+        </>
+        
     );
 }
 
--- src/app/components/Dialog/Dialog.styled.js
+++ src/app/components/Dialog/Dialog.styled.js
@@ -2,15 +2,22 @@ import styled from "styled-components";
 import css from "styled-jsx/css";
 
 export const DialogWrapper = styled.div`
+    background-color: white;    
     width: 25rem;
     margin: 0 auto;
     padding: 1rem;
     border: 1px solid #cccc;
     border-radius: .5rem;
     display: none;
+    /* position the div in center */
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
 
     ${({isOpen}) => isOpen && css`
         display: block;
+        z-index: 3;
     ` }
 `;
 
@@ -24,4 +31,12 @@ export const DialogHead = styled.div`
 `;
 
 export const DialogActions = styled.div`
+`;
+
+export const DialogBackdrop = styled.div`
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    background-color: rgb(0 0 0 / 50%);
 `;
