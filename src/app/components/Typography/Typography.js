d07c1300b48909fae50ffdbeeb4d101005d17066
Marilyn Negrete <marilyn.negrete@globant.com>
2024-01-26 17:31:58 -0500

    fix warning Typography is not a styled component

--- src/app/components/Typography/Typography.js
+++ src/app/components/Typography/Typography.js
@@ -1,23 +1,22 @@
 import { HeroHeading, SecondaryHeading, SubHeading} from "./Typography.styled";
-import { Jua, Kalam } from 'next/font/google';
-
-// fonts
-const jua = Jua({ subsets: ['latin'], weight: '400'});
-const kalam = Kalam({ subsets: ['latin'], weight: '300'});
 
 const Typography = (props) => {
     const { variant, label, className } = props;
+    let element;
 
     switch (variant) {
         case 'h1':
-          return <HeroHeading className={className}>{label}</HeroHeading>;
+          element = <HeroHeading className={className}>{label}</HeroHeading>;
         case 'h3': 
-          return <SecondaryHeading className={className}>{label}</SecondaryHeading>
+          element =  <SecondaryHeading className={className}>{label}</SecondaryHeading>
         case 'h4':
-          return <SubHeading className={className} bottomSpacing={1}>{label}</SubHeading>;
+          element =  <SubHeading className={className} bottomSpacing={1}>{label}</SubHeading>;
         default:
-          return <p className={className}>{label}</p>; // Default to a paragraph if variant is not recognized
+          element =  <p className={className}>{label}</p>; // Default to a paragraph if variant is not recognized
       }
+
+
+      return element;
 }
 
 
