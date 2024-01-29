01ce9ea8f43b21c39eb6fdf38da85d727551c3fd
Marilyn Negrete <marilyn.negrete@globant.com>
2024-01-29 18:05:59 -0500

    configure environment variables

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const path = require('path')
 const nextConfig = {
     sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
+    env: {
+      API_URL: "https://api.unsplash.com/",
+      ACCESS_KEY: "BeQb4D4iCQO3mzSw5bywCnLdNNSA1oD4Jo9GameRxaA",
+      SECRETE_KEY: "3Qb_EloCNENroKzWu3VUDlhJ7ltLmptYgdYzQ5XOU6o"
+    }
   }
 }
 
